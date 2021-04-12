2a20a06b9adcf5e18a957bec46b4259f8a8a39ae
Clark Kampfe <clark.kampfe@gmail.com>
2021-04-12 00:36:29 -0500

    add code

---
+++ README.md
@@ -0,0 +1,62 @@
+# s3dl
+
+===
+
+## install
+
+```
+$ cargo install --git https://github.com/ckampfe/s3dl
+```
+
+## example
+
+```
+$ cat my_s3_files.txt
+a.stl
+b.stl
+c.png
+d.png
+e.rs
+$ mkdir out
+$ s3dl --bucket mybucket --keys-path my_s3_files.txt -o out
+```
+
+## use/options
+
+```
+clark@doomguy:~/code/personal/s3dl$ ./target/release/s3dl -h
+s3dl 0.1.0
+Download files from S3 in parallel
+
+USAGE:
+    s3dl [OPTIONS] --bucket <bucket> --keys-path <keys-path> --out-path <out-path>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -b, --bucket <bucket>                                      The target S3 bucket
+    -k, --keys-path <keys-path>
+            A path to a newline-separated file of AWS S3 keys to download. The keys should be relative, like
+            `a/path/to/a/file.jpg`
+    -m, --max-inflight-requests <max-inflight-requests>
+            The maximum number of inflight tasks. Defaults to (number of cpus * 10)
+
+    -o, --out-path <out-path>                                  Where the downloaded files should be written
+    -r, --region <region>
+            The AWS region. Overrides the region found using the provider chain
+
+        --stderr-channel-capacity <stderr-channel-capacity>
+            The size of the channel that synchronizes writes to stderr. You generally shouldn't need to worry about this
+            [default: 100]
+        --stdout-channel-capacity <stdout-channel-capacity>
+            The size of the channel that synchronizes writes to stdout. You generally shouldn't need to worry about this
+            [default: 100]
+```
+
+## todo
+
+- [ ] options for what to do when a file already exists? replace/ignore/error
+- [ ] quiet mode?
+- [ ] logging instead of echo to stdout?
---
+++ src/main.rs
@@ -0,0 +1,187 @@
+use anyhow::{Context, Result};
+use rusoto_core::Region;
+use rusoto_s3::S3;
+use std::io::BufRead;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+use structopt::*;
+use tokio::io::AsyncWriteExt;
+use tokio::sync::{mpsc, Semaphore};
+
+const DEFAULT_CHANNEL_BUFFER_CAPACITY: &str = "100";
+
+/// Download files from S3 in parallel
+#[derive(StructOpt)]
+struct Options {
+    /// The target S3 bucket.
+    #[structopt(long, short)]
+    bucket: String,
+
+    /// A path to a newline-separated file of AWS S3 keys to download.
+    /// The keys should be relative, like `a/path/to/a/file.jpg`
+    #[structopt(long, short)]
+    keys_path: PathBuf,
+
+    /// Where the downloaded files should be written.
+    #[structopt(long, short)]
+    out_path: PathBuf,
+
+    /// The maximum number of inflight tasks.
+    /// Defaults to (number of cpus * 10)
+    #[structopt(long, short)]
+    max_inflight_requests: Option<usize>,
+
+    /// The AWS region. Overrides the region found using the provider chain.
+    #[structopt(long, short)]
+    region: Option<Region>,
+
+    /// The size of the channel that synchronizes writes to stdout.
+    /// You generally shouldn't need to worry about this.
+    #[structopt(long, default_value = DEFAULT_CHANNEL_BUFFER_CAPACITY)]
+    stdout_channel_capacity: usize,
+
+    /// The size of the channel that synchronizes writes to stderr.
+    /// You generally shouldn't need to worry about this.
+    #[structopt(long, default_value = DEFAULT_CHANNEL_BUFFER_CAPACITY)]
+    stderr_channel_capacity: usize,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let options = Options::from_args();
+
+    let (stdout_s, mut stdout_r) = mpsc::channel(options.stdout_channel_capacity);
+    let (stderr_s, mut stderr_r) = mpsc::channel(options.stderr_channel_capacity);
+
+    let mut stdout = tokio::io::stdout();
+    let mut stderr = tokio::io::stderr();
+
+    let stdout_task = tokio::spawn(async move {
+        while let Some(m) = stdout_r.recv().await {
+            let m: String = m;
+            let bytes = m.as_bytes();
+            stdout.write_all(bytes).await.unwrap();
+        }
+    });
+
+    let stderr_task = tokio::spawn(async move {
+        while let Some(m) = stderr_r.recv().await {
+            let m: String = m;
+            stderr.write_all(m.as_bytes()).await.unwrap();
+        }
+    });
+
+    let region = if let Some(region) = options.region {
+        region
+    } else {
+        rusoto_core::Region::default()
+    };
+
+    let max_inflight_requests = options
+        .max_inflight_requests
+        .unwrap_or_else(|| num_cpus::get() * 10);
+
+    let client = rusoto_s3::S3Client::new(region);
+
+    let bucket = options.bucket;
+
+    download_keys(
+        client,
+        bucket,
+        options.keys_path,
+        options.out_path,
+        stdout_s,
+        stderr_s,
+        max_inflight_requests,
+    )
+    .await?;
+
+    stdout_task.await?;
+    stderr_task.await?;
+
+    Ok(())
+}
+
+async fn download_keys(
+    client: rusoto_s3::S3Client,
+    bucket: String,
+    keys_path: PathBuf,
+    out_path: PathBuf,
+    stdout_s: mpsc::Sender<String>,
+    stderr_s: mpsc::Sender<String>,
+    max_inflight_requests: usize,
+) -> Result<()> {
+    let keys = std::fs::File::open(&keys_path)?;
+    let keys = std::io::BufReader::new(keys);
+
+    let keys_lines = keys.lines();
+
+    let mut reqs = Vec::new();
+
+    for line in keys_lines {
+        let key = line?;
+        reqs.push((
+            key.clone(),
+            rusoto_s3::GetObjectRequest {
+                bucket: bucket.clone(),
+                key,
+                ..Default::default()
+            },
+        ))
+    }
+
+    let mut tasks = Vec::with_capacity(reqs.len());
+
+    let semaphore = Arc::new(Semaphore::new(max_inflight_requests));
+
+    for (key, req) in reqs {
+        // these are all just copying pointers
+        let stdout_s = stdout_s.clone();
+        let stderr_s = stderr_s.clone();
+        let mut out = out_path.clone();
+        let client = client.clone();
+
+        let permit = Arc::clone(&semaphore).acquire_owned().await?;
+
+        let task = tokio::spawn(async move {
+            let _permit = permit;
+
+            stdout_s.send(format!("{}: started\n", key)).await.unwrap();
+
+            match client.get_object(req).await {
+                Ok(resp) => {
+                    let filename = Path::new(&key).file_name().unwrap();
+
+                    out.push(filename);
+
+                    let mut file = tokio::fs::File::create(&out)
+                        .await
+                        .with_context(|| format!("Could not create local file: {:?}", out))
+                        .unwrap();
+
+                    if let Some(body) = resp.body {
+                        let mut async_body = body.into_async_read();
+                        match tokio::io::copy(&mut async_body, &mut file).await {
+                            Ok(_) => stdout_s.send(format!("{}: finished\n", key)).await.unwrap(),
+                            Err(e) => stderr_s.send(format!("{}: {:?}", key, e)).await.unwrap(),
+                        };
+                    } else {
+                        stderr_s
+                            .send(format!("{}: response body was empty\n", key))
+                            .await
+                            .unwrap()
+                    }
+                }
+                Err(e) => stderr_s.send(format!("{}: {:?}", key, e)).await.unwrap(),
+            }
+        });
+
+        tasks.push(task);
+    }
+
+    for task in tasks {
+        task.await?
+    }
+
+    Ok(())
+}
